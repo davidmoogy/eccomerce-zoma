0c2a05a66ebea7f1f95a229546a6876b5f82e439
dato <davidmoogymoogy@gmail.com>
2026-05-18 11:43:39 -0700

    dawyeba proectis

---
+++ components/Category.jsx
@@ -0,0 +1,14 @@
+
+import React from "react";
+
+export default function Category(){
+    return(
+        <div className="categoty-buttons">
+            <button className="cleaning">Cleaning</button>
+            <button className="kitchen">Kitchen</button>
+            <button className="Bathroom">Bathroom</button>
+            <button className="paper">Paper</button>
+            <button className="rags">rags</button>
+       </div>
+    )
+}
---
+++ screens/Home.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Header from "../components/Header";
+import Category from "../components/Category";
+
+export default function Home() {
+  return (
+    <div className="main">
+      <Header />
+      <Category/>
+
+      <div className="products-container">
+          
+      </div>
+
+       
+    </div>
+  );
+}
